29b47390e395fa415fa128bf7093e0d88bb8b34a
Oleksiy Kost <AlekseyKo2208@gmail.com>
2025-02-03 00:00:43 +0200

    task-9

---
+++ src/js/2-form.js
@@ -0,0 +1,52 @@
+const STORAGE_KEY = 'feedback-form-state';
+const formData = { email: '', message: '' };
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('input', e => {
+  const email = e.currentTarget.elements.email.value.trim();
+  const message = e.currentTarget.elements.message.value.trim();
+  const formData = { email, message };
+  saveToLS(STORAGE_KEY, formData);
+});
+
+function initPage() {
+  const localData = loadFromLS(STORAGE_KEY);
+  refs.form.elements.email.value = localData?.email || '';
+  refs.form.elements.message.value = localData?.message || '';
+}
+initPage();
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = e.currentTarget.elements.email.value.trim();
+  const message = e.currentTarget.elements.message.value.trim();
+  const formData = { email, message };
+
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  e.target.reset();
+});
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+  const body = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(body);
+    return data;
+  } catch {
+    return body;
+  }
+}
